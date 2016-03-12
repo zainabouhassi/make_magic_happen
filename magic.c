1b89ed969e82103106956b977d4ba520f64e2f20
Julien Barbier <write0@gmail.com>
2016-03-11 19:17:44 -0800

    Update magic.c

--- magic.c
+++ magic.c
@@ -10,7 +10,7 @@ int main(void) {
   /*
    * write your line of code here...
    * Remember:
-   * - you are now allowed to use a
+   * - you are not allowed to use a
    * - you are not allowed to modify p
    * - only one statement
    * - you are not allowed to code anything else than this line of code
